63abdf3290657591fe1226f2dc0474d46a6f3f44
Juan Diego Cobo Cabal <desarrollador2@plafilm.com>
2023-10-05 12:46:25 -0500

    fix/profile-route-again

--- mixins/routes.js
+++ mixins/routes.js
@@ -3,16 +3,16 @@ export const loginUrl = '/api/account/login'
 export const logoutUrl = '/api/account/logout'
 export const resetPasswordUrl = '/api/account/reset-password'
 export const photoUrl = '/api/account/photo'
-export const profileUrl = '/api/account/profile/'
+export const profileUrl = '/api/account/profile'
 
 // User
-export const userUrl = '/api/user/'
+export const userUrl = '/api/user'
 
 // Teacher
-export const teacherUrl = '/api/teacher/'
+export const teacherUrl = '/api/teacher'
 
 // Student
-export const studentUrl = '/api/student/'
+export const studentUrl = '/api/student'
 
 // Permissions
-export const permissionUrl = '/api/permission/'
+export const permissionUrl = '/api/permission'
