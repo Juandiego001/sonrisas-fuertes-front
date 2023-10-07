3d057000e02c135c06234679981525da893a1738
Juandiego001 <juandiego14012003@gmail.com>
2023-10-07 08:59:24 -0500

    fix/public

--- middleware/public.js
+++ middleware/public.js
@@ -2,7 +2,9 @@ export default async function ({ redirect, $axios, app }) {
   try {
     const me = await $axios.$get('/api/account/profile')
     if (me.username) {
-      redirect(app.localePath('/'))
+      // eslint-disable-next-line no-console
+      console.log('LLEGO AQUI')
+      app.router.push('/')
     }
   } catch (err) {
   }
